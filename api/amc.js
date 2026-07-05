6644dc189cbf1957e3947f97f726650e87599060
fianreztu <161385389+fianreztu@users.noreply.github.com>
2026-07-05 14:50:15 +0800

    Comment out fetch logic and add placeholder response
    Comment out the original fetch logic and response handling, and add a placeholder response.

--- api/amc.js
+++ api/amc.js
@@ -1,19 +1,20 @@
 export default async function handler(req, res) {
-  try {
-    const page = await fetch("https://vileembeds.pages.dev/embed/amc-usa");
-    const html = await page.text();
+  //try {
+  //  const page = await fetch("https://vileembeds.pages.dev/embed/amc-usa");
+   // const html = await page.text();
 
-    const match = html.match(/https.*\.m3u8[^"]+/);
-    if (!match) {
-      return res.status(500).send("Tidak menemukan link m3u8");
+   // const match = html.match(/https.*\.m3u8[^"]+/);
+  //  if (!match) {
+  //    return res.status(500).send("Tidak menemukan link m3u8");
     }
 
-    const m3u8Url = match[0];
-    const m3u8Data = await fetch(m3u8Url).then(r => r.text());
+    //const m3u8Url = match[0];
+  //  const m3u8Data = await fetch(m3u8Url).then(r => r.text());
 
-    res.setHeader("Content-Type", "application/vnd.apple.mpegurl");
-    res.send(m3u8Data);
-  } catch (err) {
-    res.status(500).send("Error: " + err.message);
+ //   res.setHeader("Content-Type", "application/vnd.apple.mpegurl");
+ //   res.send(m3u8Data);
+//  } catch (err) {
+   // res.status(500).send("Error: " + err.message);
+  res.send("fakyu");
   }
 }
